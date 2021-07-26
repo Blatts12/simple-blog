8be2bab928eb0ea9133c856608eb8150efc8cdfb
Jakub <9402720+Blatts12@users.noreply.github.com>
2021-07-26 20:27:07 +0200

    add cssloader and styleloader for react-bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,18 @@ module.exports = {
           loader: "babel-loader",
         },
       },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
+          },
+        ],
+      },
     ],
   },
   output: {
